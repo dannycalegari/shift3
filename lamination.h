d1ad0511a219283ac0b98011d2680405e7280f88
Danny Calegari <dannyc@math.uchicago.edu>
2023-09-07 09:59:55 -0500

    Add files via upload

--- lamination.h
+++ lamination.h
@@ -4,3 +4,5 @@ struct leaf {
 	double height;		// should be > 1
 	double angle[2];	// should be in the interval [-pi,pi]
 };
+
+std::vector<leaf> dynamical_lamination(int depth, std::array<leaf,2> C);
